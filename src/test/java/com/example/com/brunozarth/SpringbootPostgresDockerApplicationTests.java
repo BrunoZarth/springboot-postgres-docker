013a31a72224b8e7de9d9ab2cff19f928664330b
Bruno <brunozarth@gmail.com>
2022-11-20 19:20:52 -0300

    first commit

---
+++ src/test/java/com/example/com/brunozarth/SpringbootPostgresDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.com.brunozarth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
